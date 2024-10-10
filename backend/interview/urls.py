9eba74d6732ceeefff241ce4fcaeb450b828535e
Jungyu Lee <jlee629@my.centennicalcollege.ca>
2024-10-10 19:00:52 -0400

    data interaction added

--- backend/interview/urls.py
+++ backend/interview/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('process_audio/', views.process_audio, name='process_audio'),
+    path('modules/', views.get_modules, name='get_modules'),
+    path('modules/<str:module_id>/', views.get_module_by_id, name='get_module_by_id'),
+
 ]
