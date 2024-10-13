ce779ed380ff55b0b694db245829b909d04123a5
hyewon6588 <hhan29@my.centennialcollege.ca>
2024-10-13 15:15:50 -0400

    login

--- backend/interview/serializers.py
+++ backend/interview/serializers.py
@@ -1,7 +1,30 @@
 from rest_framework import serializers
-from .models import Module, Users
+from .models import Module, Users,Admin
 
 class ModuleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Module
         fields = "__all__"
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+        
+        try:
+            user = Users.objects.get(email=email)
+        except Users.DoesNotExist:
+            raise serializers.ValidationError("Invalid credentials")
+
+        if not user.check_password(password):
+            raise serializers.ValidationError("Invalid credentials")
+
+       
+        is_admin = Admin.objects.filter(userid=user).exists()
+
+        attrs['user'] = user
+        attrs['is_admin'] = is_admin
+        return attrs
--- backend/interview/urls.py
+++ backend/interview/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
+    path('login/',views.login,name='login'),
     path('process_audio/', views.process_audio, name='process_audio'),
     path('modules/', views.get_modules, name='get_modules'),
     path('modules/<str:module_id>/', views.get_module_by_id, name='get_module_by_id'),
