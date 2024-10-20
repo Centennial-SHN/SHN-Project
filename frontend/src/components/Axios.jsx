e679cc3859942fae99c305ab790085504a1e2ae5
AriyaAgni <122389887+AriyaAgni@users.noreply.github.com>
2024-10-20 18:43:50 -0400

    .env reverted

---
+++ frontend/src/components/Axios.jsx
@@ -0,0 +1,19 @@
+import  axios  from 'axios';
+
+const baseURL = 'http://localhost:8080/api';
+const isDevelopment = process.env.NODE_ENV === 'development';
+const baseURL2 = isDevelopment 
+    ? import.meta.env.VITE_API_BASE_URL_LOCAL 
+    : import.meta.env.VITE_API_BASE_URL_PROD; // No quotes here
+
+
+const AxiosInstance = axios.create({
+    baseURL: baseURL2,
+    timeout: 5000,
+    headers: {
+        'Content-Type': 'application/json',
+        accept: 'application/json',
+    }
+});
+
+export default AxiosInstance;
