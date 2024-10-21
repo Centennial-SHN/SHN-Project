72819d952ca4283c5f4ec01004afce327f6b3547
AriyaAgni <122389887+AriyaAgni@users.noreply.github.com>
2024-10-20 20:34:01 -0400

    constants.js edited

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
 export const VITE_API_BASE_URL_LOCAL = 'http://localhost:8000'
-export const VITE_API_BASE_URL_PROD = 'https://shn-dajango-react-app-backend.azurewebsites.net/api/login/'  
+export const VITE_API_BASE_URL_PROD = 'https://shn-dajango-react-app-backend.azurewebsites.net'  
 
 
