acfe10ea6a69d1db068deb9e16ac41094848daca
hyewon6588 <hhan29@my.centennialcollege.ca>
2024-10-28 06:10:16 -0400

    token

--- backend/patient_interview_app/deployment.py
+++ backend/patient_interview_app/deployment.py
@@ -9,6 +9,7 @@ SECRET_KEY=os.environ['MY_SECRET_KEY']
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -16,13 +17,27 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "https://ashy-stone-000b7c90f.5.azurestaticapps.net",
 ]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://ashy-stone-000b7c90f.5.azurestaticapps.net"
+]
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+
+SESSION_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SECURE = True  # Set to True for HTTPS, False for local testing
+CSRF_COOKIE_SAMESITE = 'None'  # Same as above for CSRF
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_AGE = 1209600
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 STORAGES = {
     "default": {
