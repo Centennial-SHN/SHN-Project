fcea61b0cf09138d74b76187c4d00060896b4449
DM Brar <dmvbrar@gmail.com>
2024-10-29 00:15:38 -0400

    Updated Navbar to include change password module

--- frontend/src/components/InterviewHistory.jsx
+++ frontend/src/components/InterviewHistory.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useParams,useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { VITE_API_BASE_URL_LOCAL, VITE_API_BASE_URL_PROD } from "../constants";
 import Cookies from 'js-cookie';
-import ChangePasswordModal from './ChangePasswordModal';
+import NavBar from "./NavBar";
+import { Typography, Layout, Space } from 'antd';
+
+const { Title } = Typography;
+const { Content } = Layout;
 
 const InterviewHistory = () => {
   const [interviews, setInterviews] = useState([]);
@@ -12,11 +14,11 @@ const InterviewHistory = () => {
   const navigate = useNavigate();
   const isDevelopment = import.meta.env.MODE === "development";
   const baseUrl = isDevelopment ? VITE_API_BASE_URL_LOCAL : VITE_API_BASE_URL_PROD;
-  const [menuOpen, setMenuOpen] = useState(false); // State for toggling the menu
-  const [iconColor, setIconColor] = useState("black");
+  // const [menuOpen, setMenuOpen] = useState(false); // State for toggling the menu
+  // const [iconColor, setIconColor] = useState("black");
   const isAdmin = sessionStorage.getItem('isSuperUser') === 'true';
 
-  const [isChangePasswordOpen, setChangePasswordOpen] = useState(false);
+  // const [isChangePasswordOpen, setChangePasswordOpen] = useState(false);
 
   const backendUrl = baseUrl;
   const csrfToken = Cookies.get('csrftoken');
@@ -83,73 +85,59 @@ const InterviewHistory = () => {
     }
   };
 
-  const handleLogout = () => {
-    sessionStorage.removeItem("userId"); // Clear userId from sessionStorage
-    navigate("/"); // Redirect to login page
-  };
+  // const handleLogout = () => {
+  //   sessionStorage.removeItem("userId"); // Clear userId from sessionStorage
+  //   navigate("/"); // Redirect to login page
+  // };
 
-  const toggleMenu = () => {
-    setMenuOpen((prev) =>{
-      setIconColor(prev ? "black" : "#4DBDB1");
-      return !prev;
-    });
-  };
+  // const toggleMenu = () => {
+  //   setMenuOpen((prev) =>{
+  //     setIconColor(prev ? "black" : "#4DBDB1");
+  //     return !prev;
+  //   });
+  // };
 
-  const handleSwitchToAdmin = () => {
-    navigate("/admin/module-list"); // Replace with the actual admin route
-  };
+  // const handleSwitchToAdmin = () => {
+  //   navigate("/admin/module-list"); // Replace with the actual admin route
+  // };
 
-  const toggleChangePasswordModal = () => {
-    setChangePasswordOpen(!isChangePasswordOpen);
-  };
+  // const toggleChangePasswordModal = () => {
+  //   setChangePasswordOpen(!isChangePasswordOpen);
+  // };
 
-  // Function to handle password change
-  const handleChangePassword = async (currentPassword, newPassword) => {
-    try {
-      const response = await fetch(`${backendUrl}/api/change-password/`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          'X-CSRFToken': csrfToken
-        },
-        credentials: 'include',
-        body: JSON.stringify({
-          current_password: currentPassword,
-          new_password: newPassword,
-        }),
-      });
+  // // Function to handle password change
+  // const handleChangePassword = async (currentPassword, newPassword) => {
+  //   try {
+  //     const response = await fetch(`${backendUrl}/api/change-password/`, {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         'X-CSRFToken': csrfToken
+  //       },
+  //       credentials: 'include',
+  //       body: JSON.stringify({
+  //         current_password: currentPassword,
+  //         new_password: newPassword,
+  //       }),
+  //     });
 
-      if (!response.ok) {
-        throw new Error("Failed to change password");
-      }
+  //     if (!response.ok) {
+  //       throw new Error("Failed to change password");
+  //     }
 
-      alert("Password changed successfully!");
-      setChangePasswordOpen(false);
-    } catch (error) {
-      console.error("Error changing password:", error);
-      alert("Failed to change password. Please try again.");
-    }
-  };
+  //     alert("Password changed successfully!");
+  //     setChangePasswordOpen(false);
+  //   } catch (error) {
+  //     console.error("Error changing password:", error);
+  //     alert("Failed to change password. Please try again.");
+  //   }
+  // };
 
 
   return (
-    <div className="interview-history">
-      <header>
-        <nav>
-          <div className="hamburger" onClick={toggleMenu}>
-            <FontAwesomeIcon icon={faBars} size="2x" color={iconColor} /> {/* Use iconColor state */}
-          </div>
-          <ul className={`nav-menu ${menuOpen ? "show" : ""}`}>
-            {/* <li onClick={() => navigate(`/interview-history/${userid}`)}>Interview History</li> */}
-            <li onClick={() => navigate("/module")}>Select Module</li>
-            <li onClick={toggleChangePasswordModal}>Change Password</li>
-            {isAdmin && ( // This line ensures the "Switch to Admin" item is shown only to admin users
-                <li onClick={handleSwitchToAdmin}>Switch to Admin</li>
-            )}
-            <li onClick={handleLogout}>Logout</li>
-          </ul>
-        </nav>
-      </header>
+    <Layout className="layoutInterviewHist">
+      <NavBar isAdmin={isAdmin} />
+      <Content className="layoutIntHistContent">
       <h1>Your Interview History</h1>
       {interviews.length === 0 ? (
         <p>No interviews found.</p>
@@ -183,12 +171,8 @@ const InterviewHistory = () => {
           </tbody>
         </table>
       )}
-      <ChangePasswordModal
-        isOpen={isChangePasswordOpen}
-        onClose={toggleChangePasswordModal}
-        onChangePassword={handleChangePassword}
-      />
-    </div>
+      </Content>
+      </Layout>
   );
 };
 export default InterviewHistory;
